48c9487bb25475763fcf55317b5e91b9edd8bd4a
vicke4 <ganapathy.v2@gmail.com>
2025-11-24 22:06:25 +0530

    add astro cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,15 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare()
 });
